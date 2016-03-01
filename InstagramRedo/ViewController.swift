23d282eaa79e4f0df6714b17b204706df0736370
aashimagarg <agarg@utexas.edu>
2016-03-01 15:59:39 -0600

    Initial Commit

---
+++ InstagramRedo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramRedo
+//
+//  Created by Aashima Garg on 3/1/16.
+//  Copyright © 2016 Aashima Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
